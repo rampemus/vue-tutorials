6c9d2a89d71af459e208a6f355a422b2d10f1d0f
rampemus <pasi.toivanen@live.com>
2020-05-18 20:13:07 +0300

    Added code challenge with vuex and store initialized

---
+++ comment-challenge/src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    comments: [
+      {
+        id: 1,
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
+      },
+      {
+        id: 2,
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
+      },
+      {
+        id: 3,
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
+      }
+    ]
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
